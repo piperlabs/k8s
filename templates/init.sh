0ad74d6f6c58269f142c89df237864457dc2ee50
Eric Fischer <eric@eurocom-x7c.lan>
2020-07-17 23:43:03 +0200

    ok

--- templates/init.sh
+++ templates/init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -e
 
+master={{ .product.master }}
+if [ "$master" = "true" ]; then
+	kubeadm init --pod-network-cidr={{ .product.pod_network_cidr }} --apiserver-advertise-address={{ ip .host.name }}
+fi
